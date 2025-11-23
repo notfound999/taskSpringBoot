04d57df56ecde99564491ee482bc7f39ac8e19f7
notfound999 <ildiamarildo91@gmail.com>
2025-11-23 18:09:11 +0100

    test

---
+++ src/main/java/com/example/demo/domain/entities/TaskList.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "task_lists")
+public class TaskList {
+
+}
---
+++ src/main/java/com/example/demo/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.example.demo.domain.entities;
+
+public enum TaskStatus {
+    OPEN,CLOSED
+}
